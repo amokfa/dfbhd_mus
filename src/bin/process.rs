a905742ed5c490f464c926a95ac81abb0aa964b5
amokfa <iaansagar@gmail.com>
2023-10-05 21:38:58 +0530

    processing logic

--- src/bin/process.rs
+++ src/bin/process.rs
@@ -1,11 +1,17 @@
+use std::collections::HashMap;
 use std::fs::File;
+use std::io::Write;
 use std::mem::size_of;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use anyhow::{Result, Context};
+use itertools::Itertools;
 use dfbhd_mus::*;
+use rayon::prelude::*;
 
 fn main() {
+    rayon::ThreadPoolBuilder::new().num_threads(32).build_global().unwrap();
+
     let mut game_dir = None;
     let mut output_dir = None;
     let mut args = std::env::args().skip(1);
@@ -25,13 +31,17 @@ fn main() {
     }
     let game_dir = game_dir.unwrap();
     let output_dir = output_dir.unwrap();
+    let _ = std::fs::remove_dir_all(&output_dir);
+    std::fs::create_dir_all(&output_dir).unwrap();
     let files = [
-        // game_dir.join("menumus.sbf"),
+        game_dir.join("menumus.sbf"),
         game_dir.join("gamemus.sbf"),
-        // game_dir.join("EXP1.sbf")
+        game_dir.join("EXP1.sbf")
     ];
     for file in files {
-        process_file(file.as_path(), output_dir.as_path());
+        if let Err(e) = process_file(file.as_path(), output_dir.as_path()) {
+            dbg!(e);
+        }
     }
 }
 
@@ -48,43 +58,64 @@ fn process_file(file: &Path, output: &Path) -> Result<()> {
     });
     let index_parsed = index
         .iter()
-        .map(|e| SBFIndexEntry {
-            ident: String::from_utf8_lossy(&e.ident.iter().take_while(|&&b| b != 0).map(|b| b.to_owned()).collect::<Vec<_>>()).to_string(),
-            z1: e.z1,
-            z2: e.z2,
-            start: e.start,
-            size: e.size,
-            block_size: e.block_size,
-            z3: e.z3,
+        .map(|index| {
+            let split_point = index.ident.iter().take_while(|b| b.is_ascii_alphabetic()).count();
+            let ident = index.ident.iter().take(split_point).map(|b| b.to_owned()).collect::<Vec<_>>();
+            let suffix = index.ident.iter().skip(split_point).take_while(|&&b| b != 0).map(|b| b.to_owned()).collect::<Vec<_>>();
+            SBFIndexEntry {
+                full: String::from_utf8_lossy(index.ident.iter().take_while(|&&b| b != 0).map(|&b| b).collect::<Vec<_>>().as_slice()).to_string(),
+                ident: String::from_utf8_lossy(ident.as_slice()).to_string(),
+                suffix: String::from_utf8_lossy(suffix.as_slice()).to_string(),
+                z1: index.z1,
+                z2: index.z2,
+                start: index.start,
+                size: index.size,
+                block_size: index.block_size,
+                z3: index.z3,
+            }
         })
         .collect::<Vec<_>>();
-    let entry1 = index_parsed.first().unwrap();
-    process_chunk(&content[entry1.start as usize..(entry1.start + entry1.size) as usize])?;
-    // dbg!(index_parsed);
+    let g = index_parsed.into_iter()
+        .group_by(|index| index.ident.to_owned())
+        .into_iter()
+        .map(|(key, group)| (key, group.collect::<Vec<_>>()))
+        .collect::<HashMap<_, _>>();
+    g.par_iter().for_each(|(prefix, es)| {
+        let mut f = std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(output.join(prefix))
+            .unwrap();
+        es.iter().map(|e| {
+            process_chunk(&content[e.start as usize..(e.start + e.size) as usize]).unwrap()
+        })
+            .for_each(|data| {
+                f.write_all(data.as_slice()).unwrap();
+                f.flush().unwrap();
+            });
+    });
+    // let e = index_parsed.iter().nth(2).unwrap().clone();
+    // std::fs::write("my.tmp.dir/parsed.raw", process_chunk(&content[e.start as usize..(e.start+e.size) as usize]).unwrap()).unwrap();
     Ok(())
 }
 
-fn process_chunk(chunk: &[u8]) -> Result<()> {
-    let mut orig_data = vec![];
+fn process_chunk(chunk: &[u8]) -> Result<Vec<u8>> {
     let mut parsed_data = vec![];
-    for chunk in array_transmute::<_, SBFChunkData>(chunk).iter().take(1) {
+    for chunk in array_transmute::<_, SBFChunkData>(chunk) {
         assert_eq!(chunk.scale1, chunk.scale2);
-        dbg!(chunk.scale1);
         for b in chunk.content {
-            orig_data.push(b);
-            parsed_data.push(process_byte(b) / ((chunk.scale1).pow(2) + 1) as i16);
+            parsed_data.push(upscale_pcm(b, chunk.scale1));
         }
     }
-    std::fs::write("my.tmp.dir/orig.raw", orig_data)?;
-    std::fs::write("my.tmp.dir/parsed.raw", array_transmute(parsed_data.as_slice()))?;
-    Ok(())
+    Ok(unsafe {std::mem::transmute(parsed_data)})
 }
 
-fn process_byte(b: u8) -> i16 {
+fn upscale_pcm(b: u8, scale: u8) -> i16 {
     let b = b as f32 - 127.5;
     let b = b / 140.0;
     let b = b * i16::MAX as f32;
-    b as i16
+    let b = b as i16 / ((scale as i16).pow(2) + 1);
+    b / 2
 }
 
 #[repr(C)]
@@ -110,9 +141,11 @@ struct SBFIndexEntryBin {
     z3: u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct SBFIndexEntry {
+    full: String,
     ident: String,
+    suffix: String,
     z1: u32,
     z2: u32,
     start: u32,
